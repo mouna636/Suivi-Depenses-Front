0fee823eb84f77a2a73804c6d234f7ec4d411f43
mouna <mounabedoui6@gmail.com>
2024-05-09 20:11:49 +0100

    admin-dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
+import { GestionDepensesModule } from './Components/gestion-depenses/gestion-depenses.module';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { AdminSidebarComponent } from './Components/admin-sidebar/admin-sidebar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    AdminSidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,GestionDepensesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
