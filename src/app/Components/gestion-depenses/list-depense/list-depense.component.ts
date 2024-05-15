222784e8ce140db72cc6969a33b1c131053f673b
Malek Chtioui <chtiouimalek001@gmail.com>
2024-05-15 15:26:01 +0100

    Depense data loading through CSV

--- src/app/Components/gestion-depenses/list-depense/list-depense.component.ts
+++ src/app/Components/gestion-depenses/list-depense/list-depense.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { DepenseService } from '../depense.service';
 import { Router } from '@angular/router';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-list-depense',
   templateUrl: './list-depense.component.html',
-  styleUrls: ['./list-depense.component.scss']
+  styleUrls: ['./list-depense.component.scss'],
 })
 export class ListDepenseComponent implements OnInit {
   depenses: any[] = [];
-  constructor(private depenseService:DepenseService,private router:Router){}
-  ngOnInit(){
-   this.getAllDepenses();
-   }
-   getAllDepenses(){
-    this.depenseService.getAllDepensessByUserId().subscribe(
-     (data) =>
-      {
- this.depenses=data;
-    })
-   }
+  constructor(
+    private depenseService: DepenseService,
+    private router: Router,
+    private papa: Papa
+  ) {}
+  ngOnInit() {
+    this.getAllDepenses();
+  }
+  getAllDepenses() {
+    this.depenseService.getAllDepensessByUserId().subscribe((data) => {
+      this.depenses = data;
+    });
+  }
   /*  deleteDepense(id:number){
     this.depenseService.deleteOffre(id).subscribe((res)=>
       {
@@ -30,4 +33,37 @@ export class ListDepenseComponent implements OnInit {
    gotToEditOffre(x: any) {
     this.router.navigate([`update-offre/${x}`]);
   } */
+  csvData: [] = [];
+  csvError = '';
+  onAddCSVData() {
+    if (this.csvData.length == 0) return;
+    this.csvData.forEach((depense: any) => {
+      if (depense.date && depense.montant && depense.description) {
+        this.depenseService.addDepense(depense).subscribe({
+          next: (data: any) => {
+            this.getAllDepenses();
+          },
+          error: (error) => {
+            console.error('There was an error!', error);
+            this.csvError = error;
+          },
+        });
+      }
+    });
+    console.log(this.csvData);
+  }
+
+  readCsvData(event: any): any {
+    const file = event.target.files[0];
+
+    if (file) {
+      this.papa.parse(file, {
+        header: true,
+        complete: (results: any) => {
+          this.csvData = results.data;
+        },
+        error: (err: any) => console.log(err),
+      });
+    }
+  }
 }
