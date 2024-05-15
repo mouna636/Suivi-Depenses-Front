7121416b80ac74bed9b16d0c425e0cb511b78c2f
Malek Chtioui <chtiouimalek001@gmail.com>
2024-05-15 13:55:54 +0100

    UI changes

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isAuthenticated: boolean = false;
+  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
 
+  logout() {
+    this.authService.logout();
+  }
 }
